31dd77432223773e65c2e578f4d45c08dc4fed38
unknown <you@example.com>
2022-08-20 02:02:15 +0200

    final fv

--- src/components/Cart_ProductCard.js
+++ src/components/Cart_ProductCard.js
@@ -61,7 +61,8 @@ class Cart_ProductCard extends Component {
     const updateAttribute = (attributeName, value) => {
         const duplicateProduct = basket.find(_product => _product.id == this.props.id && Object.keys(_product.chosenAttributes)
         .every(_attribute => {
-            return _product.chosenAttributes[_attribute] === (_attribute === attributeName) ? value : this.props.chosenAttributes[_attribute]
+            const selectedAttributeValue = (_attribute === attributeName) ? value : this.props.chosenAttributes[_attribute];
+            return _product.chosenAttributes[_attribute] === selectedAttributeValue
         }))
 
         if(duplicateProduct){
